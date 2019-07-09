5a39619c374482bb2091266df9863160cf7d9b28
Quach Dang Tien <tienquach36@gmail.com>
2019-07-09 12:11:16 +0700

    4

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import Home from './components/home/home';
 import Top from './components/top/top';
 import Profile from './components/profile/profile';
 import Login from './components/login/login';
-<<<<<<< HEAD
-import Mess from './components/messenger/mess_left'
-=======
+import Mess from './components/messenger/mess_left';
 import People from './components/people/people';
 import Notfound from './components/notfound/Notfound';
->>>>>>> 53935cab93a0d81fee2458e4ac77ea32e5f9325b
 import './App.css';
 
 function App() {
@@ -26,12 +23,8 @@ function App() {
           <Switch> 
             <Route path="/home" component={Home}></Route>
             <Route path="/profile" component={Profile}></Route>
-<<<<<<< HEAD
             <Route path="/mess_left" component={Mess}></Route>
-=======
-            
             {/* <Route path="/people" component={People}></Route> */}
->>>>>>> 53935cab93a0d81fee2458e4ac77ea32e5f9325b
           </Switch>
           </>
           <Route component={Notfound}></Route>
